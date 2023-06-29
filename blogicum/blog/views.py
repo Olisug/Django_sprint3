f4b65948b5232965fae777741a51d45b62a94754
Евгений Перегудов <peregudov.e99@inbox.ru>
2023-06-29 22:34:14 +0300

    create models, admin; change templates and views

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post, Category
+
 LIMIT_OF_POSTS: int = 5
 
 
 def index(request):
     "Открывает главную страницу - Лента записей"
     template = 'blog/index.html'
-    posts = Post.objects.filter(is_published=True,
-                                pub_date__lte=timezone.now(),
-                                category__is_published=True).order_by(
-     '-pub_date'
-         )[:LIMIT_OF_POSTS]
+    posts = Post.objects.filter(
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True).order_by('-pub_date')[:LIMIT_OF_POSTS]
     context = {'post_list': posts}
     return render(request, template, context)
 
@@ -33,17 +33,16 @@ def post_detail(request, post_id):
 def category_posts(request, category_slug):
     "Открывает описание категории"
     template = 'blog/category.html'
-    category = get_object_or_404(Category,
-                                 slug=category_slug,
-                                 is_published=True)
-    category_posts = Post.objects.select_related('author',
-                                                 'location',
-                                                 'category').filter(
-                                                    is_published=True,
-                                                    category=category,
-                                                    pub_date__lte=timezone.now(
-                                                    )
-                                                    )
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True)
+    category_posts = Post.objects.select_related(
+        'author',
+        'location',
+        'category').filter(is_published=True,
+                           category=category,
+                           pub_date__lte=timezone.now())
     context = {'category': category,
                'post_list': category_posts}
     return render(request, template, context)
